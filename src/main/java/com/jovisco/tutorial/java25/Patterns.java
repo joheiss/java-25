bb51959f8f9fd33f391b98360d12586be628c5c0
joheiss <josef.heiss@jovisco.de>
2025-11-01 17:36:35 +0100

    pattern matching

--- src/main/java/com/jovisco/tutorial/java25/Patterns.java
+++ src/main/java/com/jovisco/tutorial/java25/Patterns.java
@@ -5,22 +5,47 @@ public class Patterns {
     void main() {
         var a = Integer.MAX_VALUE;
         IO.println(Patterns.what(a));
+        IO.println(Patterns.whatIf(a));
+
         var b = Long.MAX_VALUE;
         IO.println(Patterns.what(b));
+        IO.println(Patterns.whatIf(b));
+
         long c = 1;
         IO.println(Patterns.what(c));
+        IO.println(Patterns.whatIf(c));
+
         var d = "A horse is a horse, is a horse ...";
         IO.println(Patterns.what(d));
+        IO.println(Patterns.whatIf(d));
+
         var e = true;
         IO.println(Patterns.what(e));
+        IO.println(Patterns.whatIf(e));
     }
 
-    public static Object what(Object that) {
+    public static String what(Object that) {
         return switch (that) {
             case Integer i -> "Integer";
             case Long l -> "Long";
             case String s -> "String";
             default -> "Something else";
         };
+    }
+
+    public static String whatIf(Object that) {
+        if (that instanceof Integer i) {
+            return "Integer".toUpperCase();
+        }
+        if (that instanceof Long l) {
+            return "Long".toUpperCase();
+        }
+        if (that instanceof String s) {
+            return "String".toUpperCase();
+        }
+        if (that instanceof Boolean b) {
+            return "Boolean".toUpperCase();
+        }
+        return "Something else".toUpperCase();
     }
 }
